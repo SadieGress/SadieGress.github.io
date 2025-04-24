bb8ca61b07a9732c9803029e05be1412f6024921
SadieGress <179837303+JayJGress@users.noreply.github.com>
2025-04-24 16:33:35 -0400

    Version 1.0.4
    - Art page/galeery upload
    - cd command (in progress! only cd art available.)
    - Fixed issue with non-commands raising an error.

---
+++ client/terminalCommands.js
@@ -0,0 +1,115 @@
+export function loadTerminal(textBox) {
+    textBox.innerHTML = `
+    <br>
+    Welcome to Kittmaus.rip! Feel free to visit any of the following links:<br><br>
+    <a href="/art">[ART]</a><br>
+    <a href="/media">[MEDIA BLOG]</a><br>
+    <a href="/writing">[WRITING]</a><br>
+    <a href="/fishing">[FISHING MINIGAME]</a><br><br>
+    Or use the search bar below.<br>
+    (Type "help" for commands!)<br><br>
+    ==========------------------------------------------------==========<br>
+    `;
+}
+
+export function cowSay(text, textBox, cowsayLength) {
+    text.splice(0,1)
+    text = text.join(" ")
+    if (text.length === 0) text = "...";
+    textBox.innerHTML += `
+    <pre>
+      ${"=".repeat(cowsayLength)}
+    | ${text} |
+      ${"=".repeat(cowsayLength)}
+           \\
+            \\
+              ^__^
+              (oo)\\_______
+              (__)\\       )\\/\\
+                  ||----w |
+                  ||     ||</pre>`
+}
+
+let versionMap = {
+    "1.0.4":`
+    - Art page/galeery upload<br>
+    - cd command (in progress! only cd art available.)<br>
+    - Fixed issue with non-commands raising an error. 
+    `,
+    "1.0.3":`
+    - Updated about background/text colors<br>
+    - setcolor command<br>
+    - Added an awesome easter egg ;)`,
+    "1.0.2":`
+    - Specific version search<br>
+    - Changelog (and command)<br>
+    - Arrow up/down in terminal possible<br>
+    - Cowsay now has a corrected width<br>
+    - Moved around where terminal functions are stored
+    `,
+    "1.0.1":`
+    - Terminal scrolling/text entry<br>
+    - help, clear, reset, version, and cowsay commands.`
+}
+
+export function version(ver=Object.keys(versionMap)[0]) {
+    if (versionMap[ver] !== undefined){
+        const formattedVer = `
+        <strong>Version ${ver}</strong><br>
+        ---------------------------------------------------<br>
+        ${versionMap[ver]}
+        <br>---------------------------------------------------
+        `;
+        return formattedVer;
+    } else {
+        return `That's not a version that exists. The current version is ${Object.keys(versionMap)[0]}.`;
+    }
+}
+
+export function changelog() {
+    let output = ""
+    for (const ver of Object.keys(versionMap)) {
+        output += `---------------------------------------------------<br><strong>${ver}</strong><br>${versionMap[ver]}<br>`;
+    }
+    output += "---------------------------------------------------";
+    return output;
+}
+
+export function setColor(r="127", g="255", b="212"){
+    if (r === "awesome") {
+        document.body.style.animation = `rainbow 6s linear 0s infinite`;
+    }
+    if (r >= 0 && r <= 255 && g >= 0 && g <= 255 && b >= 0 && b <= 255){
+        document.body.style.animation = "none";
+        document.body.style.color = `rgb(${r}, ${g}, ${b})`;
+    }
+}
+
+export function helptext() {
+    return `<strong>Commands</strong>:<br>
+    help: shows this text.<br>
+    clear: clear the terminal.<br>
+    reset: reset terminal to initial display.<br>
+    cd &ltdir&gt: go to directory.<br>
+    version [version]: show current or specific site version.<br>
+    changelog: show site version history.<br><br>
+    cowsay [text]: moo!<br>
+    setcolor [r g b]: sets site font color.
+    `
+}
+
+export function cd(dir) {
+    const dirs = Object.create(null, {
+        "art": { value: "/art", enumerable: true },
+        "media": { value: "/media", enumerable: true },
+        "writing": { value: "/writing", enumerable: true },
+        "fishing": { value: "/fishing", enumerable: true }
+      });
+    
+    if (Object.hasOwn(dirs, dir)) {
+        window.location.href = dirs[dir];
+        return("Directory changed.")
+    } else {
+        return("Cannot find path.")
+    }
+}
